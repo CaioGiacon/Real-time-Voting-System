0927ad90ff874d05b3355bc34e9192c98dfeb622
CaioGiacon <caiofreire749@gmail.com>
2026-07-24 17:16:30 -0300

    Criando o método para salvar o usuário

--- src/main/java/com/caio/real_time_vote_system/service/UserService.java
+++ src/main/java/com/caio/real_time_vote_system/service/UserService.java
@@ -1,7 +1,19 @@
 package com.caio.real_time_vote_system.service;
 
+import com.caio.real_time_vote_system.infraestructure.entity.User;
+import com.caio.real_time_vote_system.infraestructure.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    public final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
 }
