2cc04dd3644e6351ecc1ac2015d1da499206c69c
CaioGiacon <caiofreire749@gmail.com>
2026-07-27 16:07:53 -0300

    Adicionando o endpoint para listar as enquetes criadas

--- src/main/java/com/caio/real_time_vote_system/controller/PollController.java
+++ src/main/java/com/caio/real_time_vote_system/controller/PollController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/votesystem")
 @RequiredArgsConstructor
@@ -29,4 +31,21 @@ public class PollController {
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(pollResponseDTO);
     }
+
+    @GetMapping("/polls")
+    public ResponseEntity<List<PollDTO>> polls() {
+       List<Poll> polls = pollService.getAllPolls();
+
+       List<PollDTO> pollResponseDTO = polls.stream().map(
+               poll -> new PollDTO(
+                       poll.getTitle(),
+                       poll.getQuestion(),
+                       poll.getStatus(),
+                       poll.getCreationDate(),
+                       poll.getEndDate(),
+                       poll.getUser().getId()
+               )).toList();
+
+        return ResponseEntity.ok(pollResponseDTO);
+   }
 }
--- src/main/java/com/caio/real_time_vote_system/dtos/PollDTO.java
+++ src/main/java/com/caio/real_time_vote_system/dtos/PollDTO.java
@@ -1,8 +1,7 @@
 package com.caio.real_time_vote_system.dtos;
 
 import java.time.LocalDate;
-import java.util.List;
 
-public record PollDTO(String title, List<String> questions, String status,
+public record PollDTO(String title, String questions, String status,
                       LocalDate creationDate, LocalDate endDate, Long userId) {
 }
